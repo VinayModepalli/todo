ebe9737478884a70d70e6ada9b2c0427c25da822
VinayModepalli <vinaykumarmodepalli@gmail.com>
2020-07-20 23:29:15 +0530

    Updates

---
+++ precache-manifest.0ec02a4345eb8cb6d2c9df6f32f613de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d326535ae2ae4e8d3f138e22674386a2",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "74c2ffe8cd98119cc665",
+    "url": "/todo/static/css/main.4522dc2f.chunk.css"
+  },
+  {
+    "revision": "b5b1f202b6a9a737039b",
+    "url": "/todo/static/js/2.b5763d74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.b5763d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c2ffe8cd98119cc665",
+    "url": "/todo/static/js/main.e1a18291.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
